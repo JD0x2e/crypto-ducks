45d05c593e2950178c9f3ad0b8c259185755781c
Jack Doy <jackdoy99@icloud.com>
2022-08-10 19:08:19 +0100

    Added CSS

---
+++ app.js
@@ -0,0 +1,30 @@
+
+// let age = prompt("What age are you?");
+
+// let message;
+
+// if (age >= 18) {
+//   message = "You're an adult!"
+// }
+// else if (age < 13) {
+//   message = "You're a child!"
+// }
+// else {
+//   message = "You're a teenager!"
+// }
+
+
+// let mood = prompt("What mood are you in? (happy or sad)");
+// let moodMessage;
+
+// if (mood === "happy") {
+//   moodMessage = "I'm pleased you're happy!";
+// }
+// else if (mood === "sad") {
+//   moodMessage = "Maybe you'll feel better tomorrow!";
+// }
+// else {
+//   moodMessage = "... I don't understand how you're feeling";
+// }
+
+// document.write(message + " You're feeling " + mood + ", " + moodMessage);
