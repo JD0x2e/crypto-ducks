ff2c10cb7b2dd055404c279c854f506f839241cb
Jack Doy <jackdoy99@icloud.com>
2022-08-11 09:18:29 +0100

    Updated JS

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
 
-// let age = prompt("What age are you?");
+let age = prompt("What age are you?");
 
-// let message;
+let message;
 
-// if (age >= 18) {
-//   message = "You're an adult!"
-// }
-// else if (age < 13) {
-//   message = "You're a child!"
-// }
-// else {
-//   message = "You're a teenager!"
-// }
+if (age >= 18) {
+  message = "You're an adult!"
+}
+else if (age < 13) {
+  message = "You're a child!"
+}
+else {
+  message = "You're a teenager!"
+}
 
+document.write(message);
 
-// let mood = prompt("What mood are you in? (happy or sad)");
-// let moodMessage;
+let mood = prompt("What mood are you in? (happy or sad)");
+let moodMessage;
 
-// if (mood === "happy") {
-//   moodMessage = "I'm pleased you're happy!";
-// }
-// else if (mood === "sad") {
-//   moodMessage = "Maybe you'll feel better tomorrow!";
-// }
-// else {
-//   moodMessage = "... I don't understand how you're feeling";
-// }
+if (mood === "happy") {
+  moodMessage = "I'm pleased you're happy!";
+}
+else if (mood === "sad") {
+  moodMessage = "Maybe you'll feel better tomorrow!";
+}
+else {
+  moodMessage = "... I don't understand how you're feeling";
+}
 
-// document.write(message + " You're feeling " + mood + ", " + moodMessage);
+document.write(message + " You're feeling " + mood + ", " + moodMessage);
+
+let speedLimit = prompt("What speed are you going?");
+
+let message;
+
+if (speedLimit < 40){
+  message = "You need to speed up!"
+} 
+else if(speedLimit > 60){ 
+  message = "You are speeding!"
+}
+else {
+  message = "You are driving fine!"
+}
+document.write(message);
